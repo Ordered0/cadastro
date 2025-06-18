08dcf65d4949f58a27ba7c9bc9f528062aad28a0
Rodrigo <rodrigocaiokell@gmail.com>
2025-06-18 08:49:45 -0300

    isso ai

--- src/main/java/cadastro/example/DatabaseConnection.java
+++ src/main/java/cadastro/example/DatabaseConnection.java
@@ -1,7 +1,6 @@
 package cadastro.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL =  
--- src/main/java/cadastro/example/Main.java
+++ src/main/java/cadastro/example/Main.java
@@ -2,6 +2,6 @@ package cadastro.example;
 
 public class Main {
     public static void main(String[] args) {
-    new PessoaDAO().cadastrarPessoa(new Pessoa("Maria"));
+    new PessoaDAO().cadastrarPessoa(new Pessoa("pedro"));
     }
 }
